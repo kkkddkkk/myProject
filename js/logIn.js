3c4b5231feebc7dce6d56f3cf8414688a418d943
kindokyeong <kindokyeong@gmail.com>
2025-05-15 00:17:38 +0900

    logInHTML 창 변환 애니메이션 완료. p태그 배치, 인풋 기능(유효성 검사, 인증번호 등)  작업 필요

--- js/logIn.js
+++ js/logIn.js
@@ -11,14 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
     findId = document.querySelector('.findId');//아이디 찾기
     findPw = document.querySelector('.findPw');//비밀번호 찾기
     console.log(idInput);
-
     //ID, PW 공란일 시 비활성화 끝.
 
     let bgBlock = document.querySelector('.bgBlock');//배경 도형
     let blackCover = document.querySelector('.blackCover');//검은 배경(애니메이션 용)
-    console.log(blackCover.style.width);
 
-    //회원가입 버튼 호버링/클릭 애니메이션 부여
+    //로그인 버튼 호버링/클릭 애니메이션 부여
     logInBtn.addEventListener('mouseenter', () => {
         recoilSpeed = 0;
         animateRecoil();
@@ -30,8 +28,123 @@ document.addEventListener('DOMContentLoaded', function () {
 
     logInBtn.addEventListener('click', () => {
         bgBlock.style.zIndex = '1';
+        position = 0;
+        width = 20;
+        acceleration = 0.22;
         animate();
+    })//로그인 버튼 애니메이션 끝끝
+
+    //회원가입 버튼 호버링/클릭 애니메이션 부여
+    let joinUs = document.querySelector('.joinUs');//회원가입 창
+
+    newId.addEventListener('mouseenter', () => {
+        recoilSpeed = 0;
+        animateRecoil();
     })
+    newId.addEventListener('mouseleave', () => {
+        recoilSpeed = 0;
+        animateBack();
+    })
+
+    newId.addEventListener('click', () => {
+        bgBlock.style.zIndex = '1';
+        position = 0;
+        width = 20;
+        acceleration = 0.22;
+        animate();
+
+        //회원가입 창 fade in
+        joinUs.classList.remove('fadeOut');
+        backToLogIn.classList.remove('fadeOut');
+        joinUs.classList.add('fadeIn');
+        backToLogIn.classList.add('fadeIn');
+    })//회원가입 버튼 애니메이션 끝.
+
+    //아이디 찾기 버튼 호버링/클릭 애니메이션 부여
+    let findIdWindow = document.querySelector('.findIdWindow');//아이디 찾기 창
+
+    findId.addEventListener('mouseenter', () => {
+        recoilSpeed = 0;
+        animateRecoil();
+    })
+    findId.addEventListener('mouseleave', () => {
+        recoilSpeed = 0;
+        animateBack();
+    })
+    findId.addEventListener('click', () => {
+        bgBlock.style.zIndex = '1';
+        position = 0;
+        width = 20;
+        acceleration = 0.22;
+        animate();
+
+        //아이디 찾기 창 fade in
+        findIdWindow.classList.remove('fadeOut');
+        backToLogIn.classList.remove('fadeOut');
+        findIdWindow.classList.add('fadeIn');
+        backToLogIn.classList.add('fadeIn');
+    })//아이디 찾기 버튼 애니메이션 끝.
+
+
+    //비밀번호 찾기 버튼 호버링/클릭 애니메이션 부여
+    let findPwWindow = document.querySelector('.findPwWindow');//비밀번호 찾기 창
+
+    findPw.addEventListener('mouseenter', () => {
+        recoilSpeed = 0;
+        animateRecoil();
+    })
+    findPw.addEventListener('mouseleave', () => {
+        recoilSpeed = 0;
+        animateBack();
+    })
+    findPw.addEventListener('click', () => {
+        bgBlock.style.zIndex = '1';
+        position = 0;
+        width = 20;
+        acceleration = 0.22;
+        animate();
+
+        //비밀번호 찾기 창 fade in
+        findPwWindow.classList.remove('fadeOut');
+        backToLogIn.classList.remove('fadeOut');
+        findPwWindow.classList.add('fadeIn');
+        backToLogIn.classList.add('fadeIn');
+    })//비밀번호 찾기 버튼 애니메이션 끝.
+
+
+    //아이디 찾기 창 - 비밀번호 찾기 버튼 클릭 애니메이션 부여
+    let findPWBtn = document.querySelector('.findPWBtn');//뒤로가기 버튼
+
+    findPWBtn.addEventListener('click', () => {
+        findIdWindow.classList.remove('fadeIn');
+        findPwWindow.classList.remove('fadeOut');
+        findIdWindow.classList.add('fadeOut');
+        findPwWindow.classList.add('fadeIn');
+    })
+
+
+
+    //뒤로가기 버튼 클릭 애니메이션 부여
+    let backToLogIn = document.querySelector('.backToLogIn');//뒤로가기 버튼
+
+    backToLogIn.addEventListener('click', () => {
+        //회원가입 창 fade out
+        joinUs.classList.remove('fadeIn');
+        joinUs.classList.add('fadeOut');
+
+        // //아이디 찾기 창 fade out
+        findIdWindow.classList.remove('fadeIn');
+        findIdWindow.classList.add('fadeOut');
+
+        //비밀번호 찾기 창 fade out
+        findPwWindow.classList.remove('fadeIn');
+        findPwWindow.classList.add('fadeOut');
+
+        backToLogIn.classList.remove('fadeIn');
+        backToLogIn.classList.add('fadeOut');
+        rewind();
+        bgBlock.style.zIndex = '0';
+    })//뒤로가기 버튼 애니메이션 끝.
 
 
 
@@ -47,15 +160,12 @@ document.addEventListener('DOMContentLoaded', function () {
         if (recoilSpeed >= 0.5) {
             return;
         }
-        console.log(recoilSpeed);
         requestAnimationFrame(animateRecoil);
     }
 
     //화면전환 애니메이션
     function animate() {
         let speed = 5;//시작 속도
-        console.log('Animating...');
-        console.log('speed:', speed, 'position:', position);
 
         speed -= acceleration;
         width += speed;
@@ -78,9 +188,33 @@ document.addEventListener('DOMContentLoaded', function () {
         if (recoilSpeed >= 0.5) {
             return;
         }
-        console.log(recoilSpeed);
         requestAnimationFrame(animateBack);
     }
+
+    //화면 되돌리기 애니메이션
+    function rewind() {
+        let speed = 1;//시작 속도
+        let localPosition = position;
+        let localWidth = width;
+
+        function reverseAni() {
+            speed += acceleration;
+            localWidth -= speed;
+            localPosition -= speed;
+
+            if (localPosition <= 0) {
+                blackCover.style.width = `20%`;
+                return;
+            }
+
+            bgBlock.querySelector('.logInBG').style.transform = `translateX(${localPosition}%)`;
+            bgBlock.querySelector('.logInBG2').style.transform = `translateX(${localPosition}%)`;
+            blackCover.style.width = `${Math.max(localWidth, 0)}%`;
+            requestAnimationFrame(reverseAni);
+        }
+        requestAnimationFrame(reverseAni);
+    }
+
 
 
 })
