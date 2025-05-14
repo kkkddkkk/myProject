ff70ba1d1c9a16c1313f1d5aa5b5a207f21f5e45
kkkddkkk <fullstack.kindokyeong@gmail.com>
2025-05-14 18:27:19 +0900

    애니메이션 작업 완료/ 회원가입 input 생성

--- js/logIn.js
+++ js/logIn.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
+
     //ID, PW 공란일 시 비활성화
-    let idInput, pwInput, logInBtn;
+    let idInput, pwInput, logInBtn, newId, findId, findPw;
     idInput = document.querySelectorAll('.IDPW')[0];//ID 입력란
     pwInput = document.querySelectorAll('.IDPW')[1];//PW 입력란
     logInBtn = document.querySelector('.logInBtn');//로그인 버튼
+    newId = document.querySelector('.newId');//회원가입
+    findId = document.querySelector('.findId');//아이디 찾기
+    findPw = document.querySelector('.findPw');//비밀번호 찾기
     console.log(idInput);
 
     //ID, PW 공란일 시 비활성화 끝.
@@ -14,54 +18,69 @@ document.addEventListener('DOMContentLoaded', function () {
     let blackCover = document.querySelector('.blackCover');//검은 배경(애니메이션 용)
     console.log(blackCover.style.width);
 
-    logInBtn.addEventListener('click', () => {
-        // bgBlock.style.transform = 'translateX(100vw)';
-        bgBlock.style.zIndex = '1';
-
-        let position = 0;
-        let width = 0;
-        let recoilSpeed = 0;
-        let speed = 0.5;//시작 속도
-        let acceleration = 0.02;//가속도
-
-        function animateRecoil() {
-            if (recoilSpeed < -5) {
-                return;
-            }
-
-            recoilSpeed -= 0.5;
-            bgBlock.style.transform = `translateX(${recoilSpeed}%)`;
-            console.log(recoilSpeed);
-            requestAnimationFrame(animateRecoil);
-        }
-
-        function animate() {
-            if (position >= 100) {
-                return;
-            }
-
-            position += speed;
-            width += speed;
-            speed += acceleration;
-
-            bgBlock.style.transform = `translateX(${position}%)`;
-            blackCover.style.width = `${width}%`;
-            requestAnimationFrame(animate);
-        }
-
+    //회원가입 버튼 호버링/클릭 애니메이션 부여
+    logInBtn.addEventListener('mouseenter', () => {
+        recoilSpeed = 0;
         animateRecoil();
-        animate();
-
-        // setInterval(() => {
-        //     position += 1;
-        //     width += 2;
-
-        //     bgBlock.style.transform = `translateX(${position}%)`;
-        //     blackCover.style.width = `${width}%`;
-        // }, timeSet);
-
-
-
-        console.log('clicked');
     })
+    logInBtn.addEventListener('mouseleave', () => {
+        recoilSpeed = 0;
+        animateBack();
+    })
+
+    logInBtn.addEventListener('click', () => {
+        bgBlock.style.zIndex = '1';
+        animate();
+    })
+
+
+
+    let position = 0;
+    let width = 20;
+    let recoilSpeed = 0;
+    let acceleration = 0.22;//감속도
+
+    //반동 애니메이션1
+    function animateRecoil() {
+        recoilSpeed += 0.03;
+        bgBlock.style.transform = `translateX(-${recoilSpeed}%)`;
+        if (recoilSpeed >= 0.5) {
+            return;
+        }
+        console.log(recoilSpeed);
+        requestAnimationFrame(animateRecoil);
+    }
+
+    //화면전환 애니메이션
+    function animate() {
+        let speed = 5;//시작 속도
+        console.log('Animating...');
+        console.log('speed:', speed, 'position:', position);
+
+        speed -= acceleration;
+        width += speed;
+        position += speed;
+
+        bgBlock.querySelector('.logInBG').style.transform = `translateX(${position}%)`;
+        bgBlock.querySelector('.logInBG2').style.transform = `translateX(${position}%)`;
+        blackCover.style.width = `${width}%`;
+
+        if (position >= 80) {
+            return;
+        }
+        requestAnimationFrame(animate);
+    }
+
+    //반동 애니메이션2
+    function animateBack() {
+        recoilSpeed += 0.03;
+        bgBlock.style.transform = `translateX(${recoilSpeed}%)`;
+        if (recoilSpeed >= 0.5) {
+            return;
+        }
+        console.log(recoilSpeed);
+        requestAnimationFrame(animateBack);
+    }
+
+
 })
