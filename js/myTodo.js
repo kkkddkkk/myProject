3156f9ff6e31ca6c50ef4e4a799e425d2f5f8a78
kkkddkkk <fullstack.kindokyeong@gmail.com>
2025-06-05 18:30:47 +0900

    checkTodo에서 subDivision 삭제 후 dropMenu value 바꾸면 다른 division의 subdivision이 display되지않음.(삭제된 상태 그대로) 이에 test.js 수정 중

--- js/myTodo.js
+++ js/myTodo.js
@@ -2,46 +2,84 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let dropDown = document.querySelector('.dropDown');//드롭다운메뉴
-    let divisionName = document.querySelector('.divisionName').children[1];//division명
+    let dropMenu = document.querySelectorAll('.dropMenu');
+    let addNewTask = document.querySelector('.addNewTask');
+
+    let divisionName = document.querySelector('.divisionName').children[1].innerHTML;//division명
+
+    let showDivision = document.querySelector('.showDivision');//division 구역
+    let divisionBtns = document.querySelector('.divisionBtns');//division 버튼 구역
 
     let subDivisionUl = document.querySelector('.subDivisionUl');//subdivision구역
     let subDivisionBtns = document.querySelector('.subDivisionBtns');//subdivision 버튼 구역
 
-    let taskUl = document.querySelectorAll('.taskUl');
+    let taskUl = document.querySelectorAll('.taskUl');//전체 task
 
-    //division이 1개 이상 있을 시, 첫번째 division을 메인으로
-    if (dropDown.value != '새 업무 추가') {
-        divisionName.innerHTML = dropDown.value;
-        subDivisionUl.classList.add(dropDown.value);
-    }
+    //모달 변수
+    let modalBGAtPage = document.querySelector('.modalBGAtPage');//모달 배경
+    let withFriends = document.querySelector('.withFriends');//친구 추가 div
+    let friendsListDiv = document.querySelector('.friendsListDiv');//친구 list 전체
+    let searchFriendDiv = document.querySelector('.searchFriendDiv');//검색 결과 list 전체
 
-    dropDown.addEventListener('change', () => {
-        if (dropDown.value != '새 업무 추가') {//SELECT 값 선택 시 해당 DIVISION으로 내용 변경
-            divisionName.innerHTML = dropDown.value;
-            subDivisionUl.classList.add(dropDown.value);
+    let diviNameInput = document.querySelector('.diviNameInput');//division 명 수정 input
+
+    let addFriendsBtn = document.querySelector('.friendsListTitle').querySelector('button');//친구 추가 버튼
+
+    //dropdown 메뉴 이벤트
+    dropDownMenu();
+
+    //divisionBtns 클릭 이벤트
+    //#region
+    divisionBtns.addEventListener('click', (event) => {
+
+        clickAddModiIcon('fa-pen-to-square', event, document.querySelector('.divisionName').children[1].innerHTML);//수정 Icon 클릭 이벤트
+        clickAddModiIcon('fa-plus', event, '');//addIcon 클릭 이벤트
+
+        if (event.target.classList.contains('fa-xmark')) {//xIcon 클릭 이벤트
+            if (confirm('정말 삭제하시겠습니까?\n삭제 후 해당 스케줄을 되돌릴 수 없습니다.')) {
+                for (let i = 0; i < dropMenu.length; i++) {
+                    //드롭메뉴 삭제
+                    if (divisionName == dropMenu[i].innerHTML) {
+                        dropDown.removeChild(dropMenu[i]);
+                        if (dropMenu[i + 1] == null) {
+                            addNewTask.selected = true;
+                        } else {
+                            dropMenu[i + 1].selected = true;
+                        }
+                    }
+
+                    dropDownMenu();//드롭메뉴 재정리
+                }
+            }
         }
-
     })
 
+    //#endregion =================================> ddivisionBtns 클릭 이벤트 끝.
+
     //subDivisionBtns 클릭 이벤트
+    //#region
+
     subDivisionUl.addEventListener('click', (event) => {
         let thisTaskUl = event.target.parentNode.parentNode.parentNode.querySelector('.taskUl');
+        let thisSubDiviName = event.target.parentNode.parentNode.querySelector('.subDivisionName').querySelector('p').innerHTML;
 
-        if (event.target.classList.contains('fa-pen-to-square')) {
+        clickAddModiIcon('fa-pen-to-square', event, thisSubDiviName);//subDivision 수정
 
-        }
-        if (event.target.classList.contains('fa-xmark')) {
-
+        if (event.target.classList.contains('fa-xmark')) {//subDivision 삭제
+            console.log(event.target.parentNode.parentNode.parentNode);
+            if (confirm('정말 삭제하시겠습니까?\n삭제 후 해당 스케줄을 되돌릴 수 없습니다.')) {
+                subDivisionUl.removeChild(event.target.parentNode.parentNode.parentNode);
+            }
         }
         if (event.target.classList.contains('fa-plus')) {
 
         }
+
+        //task 펼치기, 접기
         if (event.target.classList.contains('fa-angle-down')) {
-            console.log('clicked');
             event.target.classList.remove('fa-angle-down');
             event.target.classList.add('fa-angle-up');//아이콘 변경
 
-            console.log(event.target.parentNode.parentNode.parentNode.querySelector('.taskUl'));
             thisTaskUl.style.maxHeight = thisTaskUl.scrollHeight + 'px';
         } else if (event.target.classList.contains('fa-angle-up')) {
             event.target.classList.remove('fa-angle-up');
@@ -51,26 +89,53 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    //#endregion =================================> subDivisionBtns 클릭 이벤트 끝.
 
-    function foldUpDown(event, displayDiv, displaySubDiv) {
-        if (event.target.classList.contains('fa-chevron-down') && displayDiv != null) {
-            event.target.classList.remove('fa-chevron-down');
-            event.target.classList.add('fa-chevron-up');//아이콘 변경
+    //모달 이벤트
 
-            if (displaySubDiv != undefined) {
-                displaySubDiv.style.maxHeight = (displaySubDiv.scrollHeight + displayDiv.scrollHeight) + 'px';
-                displayDiv.style.maxHeight = displayDiv.scrollHeight + 'px';
+
+    //function
+    //#region[function]
+
+    //dropDownMenu()
+    function dropDownMenu() {
+        //division이 1개 이상 있을 시, 첫번째 division을 메인으로
+        if (dropDown.value != '새 업무 추가') {
+            divisionName = dropDown.value;
+            subDivisionUl.classList.add(dropDown.value);
+        } else {
+            showDivision.style.display = 'none';
+            subDivisionUl.style.display = 'none';
+        }
+
+        dropDown.addEventListener('change', () => {
+            if (dropDown.value != '새 업무 추가') {//SELECT 값 선택 시 해당 DIVISION으로 내용 변경
+                document.querySelector('.divisionName').children[1].innerHTML = dropDown.value;
+                subDivisionUl.classList.add(dropDown.value);
+
+                showDivision.style.display = 'flex';
+                subDivisionUl.style.display = 'block';
             } else {
-                displayDiv.style.maxHeight = displayDiv.scrollHeight + 'px';
+                showDivision.style.display = 'none';
+                subDivisionUl.style.display = 'none';
             }
+        })
+    }
 
-        } else if (event.target.classList.contains('fa-chevron-up') && displayDiv != null) {
-            event.target.classList.remove('fa-chevron-up');
-            event.target.classList.add('fa-chevron-down');
+    //clickAddModiIcon(icon, event, name)
+    function clickAddModiIcon(icon, event, name) {
+        if (event.target.classList.contains(icon)) {
+            //모달 setting
+            modalBGAtPage.style.display = 'block';
+            withFriends.style.display = 'flex';
+            friendsListDiv.style.display = 'block';
+            searchFriendDiv.style.display = 'none';
 
-            displayDiv.style.maxHeight = null;
+            diviNameInput.value = name;
+
         }
     }
 
+    //#endregion
 
 })
--- js/testForMyTodo.js
+++ js/testForMyTodo.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //dropdown menu 랜덤 생성
     let ranNum = parseInt(Math.random() * 5);
     let dropDown = document.querySelector('.dropDown');
     let dropMenu = document.querySelector('.dropMenu');
     let addNewTask = document.querySelector('.addNewTask');
-    let clone;
+    let innerWrap = document.querySelector('.innerWrap');
 
+    let subDivisionUl = document.querySelector('.subDivisionUl');
+    let subDivisionLi = document.querySelector('.subDivisionLi');
+    let clone, cloneDiv, ranNum2;
+
+    //division 랜덤생성
     for (let i = 0; i < ranNum; i++) {
         clone = dropMenu.cloneNode(true);
         clone.innerHTML = `division${i + 1}`;
@@ -14,18 +18,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
         //addNewTask 위에 clone 붙이기
         dropDown.insertBefore(clone, addNewTask);
-    }
 
+        ranNum2 = parseInt(Math.random() * 5 + 1);
+        cloneDiv = subDivisionUl.cloneNode(true);
+        cloneDiv.classList.add(clone.value);
+        cloneDiv.style.display = 'block';
+        innerWrap.appendChild(cloneDiv);
+        console.log(innerWrap.innerHTML);
+
+    }
     dropMenu = document.querySelectorAll('.dropMenu');
-    if(ranNum!=0){
+    if (ranNum != 0) {
         addNewTask.selected = false;
         dropMenu[1].selected = true;
+
     }
 
     //subDivision 랜덤 생성
-    ranNum = parseInt(Math.random() * 5 + 1);
-    let subDivisionUl = document.querySelector('.subDivisionUl');
-    let subDivisionLi = document.querySelector('.subDivisionLi');
+    
 
     for (let i = 0; i < ranNum; i++) {
         clone = subDivisionLi.cloneNode(true);
@@ -52,6 +62,29 @@ document.addEventListener('DOMContentLoaded', () => {
             subDivisionLi[i].querySelector('.taskUl').appendChild(clone);
         }
     }
+
+    //팀원 랜덤생성
+    let friendsListDiv = document.querySelector('.friendsListDiv');
+    let friendsList = document.querySelector('.friendsList');
+    ranNum = parseInt(Math.random() * 20 + 1);
+    for (let i = 0; i < ranNum; i++) {
+        clone = friendsList.cloneNode(true);
+        clone.style.display = 'flex';
+        clone.classList.add(`friend${i + 1}`);
+        clone.querySelector('p').innerHTML = `friend${i + 1}`;
+        friendsListDiv.appendChild(clone);
+    }
+
+    let searchListDiv = document.querySelector('.searchListDiv');
+    let searchList = document.querySelector('.searchList');
+    for (let i = 0; i < ranNum; i++) {
+        clone = searchList.cloneNode(true);
+        clone.style.display = 'flex';
+        clone.classList.add(`name${i + 1}`);
+        clone.querySelector('p').innerHTML = `name${i + 1}`;
+        searchListDiv.appendChild(clone);
+    }
+
 
 
 })
