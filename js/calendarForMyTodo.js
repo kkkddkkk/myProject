cc0cb58ec3ff96732ee8c7f3bbd8cd534959c799
kkkddkkk <fullstack.kindokyeong@gmail.com>
2025-06-11 18:25:53 +0900

    달력... 매주 이벤트 구현 중 일자 클릭 -> 매주 버튼 클릭 -> 추가된 일자 해제 시 이벤트 미발생. 일자 클릭 이벤트에 조건 점검할 것

--- js/calendarForMyTodo.js
+++ js/calendarForMyTodo.js
@@ -4,8 +4,10 @@ const date = new Date();//현재날짜
 let thisYear = date.getFullYear(); //임시 년도 변수
 let thisMonth = date.getMonth(); //임시 월별 변수
 let day;//임시 요일 변수
-let checkedDate = new Array();
+let checkedDate = new Array();//선택 날짜 배열
+let dayArray = new Array();//매주 반복 시 요일 배열
 let num = 0;
+let timeChk = false;
 
 let calendarTable = document.querySelector('table');
 let tBody = document.querySelector('tbody');
@@ -24,29 +26,61 @@ document.querySelector('.month').innerHTML = thisMonth + 1;
 calander();
 let cells = document.querySelectorAll('td');
 
+
 // 요일 체크 이벤트
+
 calendarTable.addEventListener('click', (event) => {
     if (event.target.tagName == 'TD' && event.target.innerHTML != '') {
         let getDateAt = event.target.getAttribute('data-date');
 
-        if (!event.target.classList.contains('blurTextColor')) {
-            if (event.target.classList.contains('pointColorForBorder')) {
-                event.target.classList.remove('pointColorForBorder')
+        if (event.target.classList.contains('blurTextColor')) return;
 
+        //재클릭 시 해당 데이터 배열에서 삭제
+        if (checkedDate.includes(getDateAt)) {
+            if (checkedDate.indexOf(getDateAt) > -1) {
+                checkedDate.splice(checkedDate.indexOf(getDateAt), 1);
+            }
+        } else if (!checkedDate.includes(getDateAt)) {//중복 데이터 방지
+            //클릭한 날짜를 checkedDate 배열에 담음.
+            checkedDate.push(getDateAt);
+        }
+        // //배열에 담긴 데이터만 달력에 표시
+        setCheckOnCalendar();
+
+        if (period[0].checked) {
+            removeRadio(0);
+        }
+
+        //매주 반복 클릭 상태일 경우
+        if (period[1].checked) {
+            loopEveryWeek();
+
+            //0.1초 뒤 체크됨 표시
+            setTimeout(() => {
+                if (checkedDate.includes(getDateAt)) {
+                    checkedDate.forEach((data) => {
+                        document.querySelector(`[data-date="${data}"]`).classList.add('checkedNow');
+                    })
+                }
+            }, 10)
+
+            console.log('클릭 이벤트 내부: ' + event.target.classList.contains('checkedNow'));
+            console.log(event.target);
+            //체크됨 표식 있을 시 매주 이벤트 삭제
+            if (event.target.classList.contains('checkedNow')) {
+                removeRadio(1);
                 if (checkedDate.indexOf(getDateAt) > -1) {
                     checkedDate.splice(checkedDate.indexOf(getDateAt), 1);
                 }
-            } else {
-                if (!checkedDate.includes(getDateAt)) {
-                    checkedDate.push(getDateAt);
-                }
             }
-        }
-        setCheckOnCalendar();
 
-        console.log(checkedDate);
+            setCheckOnCalendar();
+        }
     }
 })
+
+
+
 
 //이동버튼 이벤트
 //#region
@@ -64,10 +98,11 @@ before.addEventListener('click', () => {
 
     initCalendar();
     calander();
-    if (period[0].checked) loopEveryDay();
-    if (period[1].checked) loopEveryWeek();
+    cells = document.querySelectorAll('td');
+    setCheckOnCalendar();
+    // if (period[0].checked) loopEveryDay();
+    // if (period[1].checked) loopEveryWeek();
 
-    checkingCalendar();
 })
 
 //다음달로 이동 버튼
@@ -83,9 +118,10 @@ after.addEventListener('click', () => {
 
     initCalendar();
     calander();
-    if (period[0].checked) loopEveryDay();
-    if (period[1].checked) loopEveryWeek();
-    checkingCalendar();
+    cells = document.querySelectorAll('td');
+    setCheckOnCalendar();
+    // if (period[0].checked) loopEveryDay();
+    // if (period[1].checked) loopEveryWeek();
 })
 
 //#endregion
@@ -104,21 +140,72 @@ deadLineCheckBox.addEventListener('click', (event) => {
 })
 
 //매일 체크 시 이벤트
-radioEvent(0, loopEveryDay);
+//#region
+
+period[0].parentNode.addEventListener('click', () => {
+    if (!period[0].checked) {
+        addRadio(0);
+        removeRadio(1);
+        removeRadio(2);
+
+        cells = document.querySelectorAll('td');
+        cells.forEach((cell) => {
+            if (!cell.classList.contains('blurTextColor') && cell.innerHTML != '') {
+                cell.classList.add('pointColorForBorder');
+            }
+        })
+    } else {
+        removeRadio(0);
+        setCheckOnCalendar();
+    }
+})
+
+//#endregion
 
 //매주 체크 시 이벤트
-radioEvent(1, loopEveryWeek);
+//#region
 
+period[1].parentNode.addEventListener('click', () => {
+    if (!period[1].checked) {
+        addRadio(1);
+        removeRadio(0);
+        removeRadio(2);
+
+        cells.forEach((cell) => {
+            if (!cell.classList.contains('blurTextColor') && cell.innerHTML != '') {
+                cell.classList.remove('checkedNow');
+            }
+        })
+
+        loopEveryWeek();
+
+        checkedDate.forEach((data) => {
+            document.querySelector(`[data-date="${data}"]`).classList.add('checkedNow');
+            console.log('매주 버튼 이벤트 내부: ' + document.querySelector(`[data-date="${data}"]`).classList.contains('checkedNow'));
+            console.log(document.querySelector(`[data-date="${data}"]`));
+        })
+
+    } else {
+        removeRadio(1);
+        setCheckOnCalendar();
+    }
+})
+
+//#endregion
 
 
 //초기화버튼 클릭 이벤트
 initBtn.addEventListener('click', () => {
+    //배열 초기화
     while (checkedDate.length) {
         checkedDate.pop();
     }
-    cells.forEach((cell) => {
-        cell.classList.remove('pointColorForBorder');
-    })
+    setCheckOnCalendar();
+
+    //버튼 초기화
+    for (let i = 0; i < period.length; i++) {
+        removeRadio(i);
+    }
 })
 
 
@@ -135,20 +222,13 @@ function calander() {
         if (testDate == null) break;
 
         switch (testDate.getDay()) {
-            case 0: day = 'sun';
-                break;
-            case 1: day = 'mon';
-                break;
-            case 2: day = 'tue';
-                break;
-            case 3: day = 'wed';
-                break;
-            case 4: day = 'thu';
-                break;
-            case 5: day = 'fri';
-                break;
-            case 6: day = 'sat';
-                break;
+            case 0: day = 'sun'; break;
+            case 1: day = 'mon'; break;
+            case 2: day = 'tue'; break;
+            case 3: day = 'wed'; break;
+            case 4: day = 'thu'; break;
+            case 5: day = 'fri'; break;
+            case 6: day = 'sat'; break;
         }
 
         for (let j = 0; j < week[num].children.length; j++) {
@@ -156,7 +236,7 @@ function calander() {
                 week[num].children[j].innerHTML = i;
                 week[num].children[j].setAttribute('data-date', `${thisYear}-${thisMonth}-${i}`)
 
-                //선택 불가능한 날짜는 흐리게 + cursor를 auto로
+                //이전 날짜는 흐리게 + cursor를 auto로
                 let targetDate = new Date(thisYear, thisMonth, i, 23, 59, 59);
                 if (date >= targetDate) {
                     week[num].children[j].classList.add('blurTextColor');
@@ -223,51 +303,31 @@ function setCheckOnCalendar() {
     checkedDate.forEach((date) => {
         if (document.querySelector(`[data-date="${date}"]`)) {
             document.querySelector(`[data-date="${date}"]`).classList.add('pointColorForBorder');
+            document.querySelector(`[data-date="${date}"]`).classList.remove('checkedNow');
         }
     })
 }
 
-
-//일정 표시 checkingCalendar();
+//removeRadio(i), addRadio(i)
 //#region
 
-function checkingCalendar() {
-    for (let i = 0; i < checkedDate.length; i++) {
-        for (let j = 0; j < document.querySelectorAll('td').length; j++) {
-            if (document.querySelectorAll('td') == null) {
-                break;
-            }
-            if (document.querySelectorAll('td')[j].getAttribute('data-date') == checkedDate[i]) {
-                document.querySelectorAll('td')[j].classList.add('pointColorForBorder');
-            }
-        }
+function removeRadio(i) {
+    period[i].checked = false;
+
+    period[i].parentNode.classList.remove('pointColorForBG');
+    period[i].parentNode.classList.add('BGColor');
+
+}
+function addRadio(i) {
+    if (!period[i].checked) {
+        period[i].checked = true;
+
+        period[i].parentNode.classList.remove('BGColor');
+        period[i].parentNode.classList.add('pointColorForBG');
     }
 }
 
-//#endregion
-
-//radioEvent(i,func)
-//#region
-
 function radioEvent(i, func) {
-
-    function removeRadio(j) {
-        period[j].checked = false;
-
-        period[j].parentNode.classList.remove('pointColorForBG');
-        period[j].parentNode.classList.add('BGColor');
-
-    }
-    function addRadio(j) {
-        if (!period[j].checked) {
-            period[j].checked = true;
-
-            period[j].parentNode.classList.remove('BGColor');
-            period[j].parentNode.classList.add('pointColorForBG');
-        }
-    }
-
-
     period[i].parentNode.addEventListener('click', () => {
 
         if (period[i].checked) {
@@ -304,45 +364,39 @@ function radioEvent(i, func) {
 //#endregion
 
 
-//매일 반복 체크 loopEveryDay();
-//#region
-
-function loopEveryDay() {
-    cells = document.querySelectorAll('td');
-    cells.forEach((cell) => {
-        if (!cell.classList.contains('blurTextColor') && cell.innerHTML != '') {
-            cell.classList.add('pointColorForBorder');
-        }
-    })
-
-}
-
-//#endregion
-
 //매주 반복 체크 loopEveryWeek();
 //#region
 
 function loopEveryWeek() {
+    let targetDate, targetDay;
     if (checkedDate[0]) {
         checkedDate.forEach((date) => {
-            let targetDate = new Date(date);
+            targetDate = new Date(date);
             targetDate.setMonth(targetDate.getMonth() + 1);
+            targetDay = targetDate.getDay();
 
-            cells.forEach((cell) => {
-                let cellDate = new Date(cell.getAttribute('data-date'));
-                cellDate.setMonth(cellDate.getMonth() + 1);
+            week.forEach((w) => {
+                let compareData = w.children[targetDay].getAttribute('data-date');
+                let compareDate = new Date(compareData);
+                compareDate.setMonth(compareDate.getMonth() + 1);
 
-                if (targetDate.getDay() == cellDate.getDay()) {
-                    if (targetDate < cellDate) {
-                        cell.classList.add('pointColorForBorder');
+                if (!w.children[targetDay]) return;
+
+                if (compareDate >= targetDate) {
+                    if (!checkedDate.includes(compareData)) {//중복 데이터 방지
+                        checkedDate.push(compareData);
                     }
                 }
             })
         })
+        setCheckOnCalendar();
 
+        if (!dayArray.includes(targetDay)) {
+            dayArray.push(targetDay);
+        }
     }
-
 }
+
 
 
 //#endregion
